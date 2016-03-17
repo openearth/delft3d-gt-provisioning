be278144513e1e54891f405a7d8f73075ec38874
Anna Van Gils <anna.vangils@deltares.nl>
2016-03-17 09:44:17 +0100

    Add watcher.py to server

--- roles/worker_python2.7/files/run.sh
+++ roles/worker_python2.7/files/run.sh
@@ -3,6 +3,5 @@
 echo "Make checkout of subversion repository"
 cd /data
 svn checkout http://10.0.1.2/repos/trunk --username=svn --password={{ subversionpass }} --no-auth-cache
-#svn checkout http://10.0.1.2/repos/trunk --username=statoil --password=statoil --no-auth-cache
 echo "start python script"
 python /data/monitoring_integrated.py $1
