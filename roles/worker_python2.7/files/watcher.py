e2a7d58159a4b282755c9c2676727f5ddf7f571f
Anna Van Gils <anna.vangils@deltares.nl>
2016-03-17 12:04:55 +0100

    Compress netcdf-file when delft3drun is finished

--- roles/worker_python2.7/files/watcher.py
+++ roles/worker_python2.7/files/watcher.py
@@ -1,36 +1,50 @@
 import logging
-import time
-import itertools
 
-from watchdog.observers import Observer
 import watchdog.events
 
 logger = logging.getLogger(__name__)
+
+
+class DoneEvent(object):
+    """Fired when done"""
+    pass
 
 
 class NetCDFHandler(watchdog.events.FileSystemEventHandler):
     """handle filesystem changes"""
     def __init__(self,  *args, **kwargs):
         super(NetCDFHandler, self).__init__(*args, **kwargs)
+        # allow methods to be added when netcdf files change
         self.processors = []
-        self.observer = None
+        # allow to extend with extra handlers when done
+        self.done_handlers = []
         # mark if we don't expect anymore changes
         self.done = False
-        self.fname_nc = ''
 
     def on_created(self, event):
         """file created on file system"""
         if isinstance(event, watchdog.events.FileCreatedEvent):
             if event.src_path.endswith('done'):
                 logger.info("we're done %s", event)
-                self.done = True
+                # raise an event
+                event = DoneEvent()
+                self.on_done(event)
+
+    def on_done(self, event):
+        """call this when done"""
+        for handler in self.done_handlers:
+            handler()
+        self.done = True
+        logger.info("All handling is done")
 
     def on_modified(self, event):
         """file modified on filesystem"""
         if isinstance(event, watchdog.events.FileModifiedEvent):
-            if event.src_path.endswith(self.fname_nc):
+            if event.src_path.endswith('.nc'):
                 for processor in self.processors:
                     processor(event.src_path)
             elif event.src_path.endswith('done'):
                 logger.info("we're done %s", event)
                 self.done = True
+                event = DoneEvent()
+                self.on_done(event)
