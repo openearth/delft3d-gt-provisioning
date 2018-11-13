73d4d94b1e78b987223b47297620efac40308cd2
Maarten Pronk <git@evetion.nl>
2018-11-13 10:20:19 +0100

    Provision cluster based on Amazon EKS.

---
+++ roles/cloud_amazon/templates/aws_sync.sh.j2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PIDFILE=/home/vagrant/forever.pid
+
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+# Execute aws sync
+source {{ django_virtualenv }}/bin/activate; cd {{ django_path }}; python manage.py scanbucket > /dev/null"
+
+rm $PIDFILE
