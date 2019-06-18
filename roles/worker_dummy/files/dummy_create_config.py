01eb055a126daf2b6030c7c5fa6a176a49973248
avgils <vangils.anna@gmail.com>
2019-06-18 11:10:00 +0200

    cleanup tags and when statements (#163)
    * cleanup tags and when statements
    
    * check current playbook
    
    * check syntax of playbooks, dryrun doesn't work with amazon tasks
    
    * update ansible version on travis
    
    * uncomment important stuff
    
    * add comment for clarity

--- roles/worker_dummy/files/dummy_create_config.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-logger.info('STARTED: Preprocessing is started. Progress is 0%.')
-logger.info('FINISHED: Preprocessing is finished. Progress is 100%.')
